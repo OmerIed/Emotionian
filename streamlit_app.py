97823103a08e9ca3f064afb87c8ff87efc1ff9f7
Omer Ied <omeried73@gmail.com>
2024-03-15 19:01:14 +0200

    Try audio changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import streamlit as st
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     st.write('Sabag has Brachydactyly type E (https://rarediseases.info.nih.gov/diseases/987/brachydactyly-type-e)')
+    audio_file = st.sidebar.file_uploader(label="",
+                                          type=[".wav", ".wave", ".flac", ".mp3", ".ogg"])
+
+    st.audio(audio_file , format="audio/wav", start_time=0)
