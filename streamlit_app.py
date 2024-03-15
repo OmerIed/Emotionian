df7fea4b47a3b31a73adefdf0b8ca5cc7960b86f
OmerIed <46245817+OmerIed@users.noreply.github.com>
2024-03-15 18:40:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    st.write('Hello world!')
+    st.write('Sabag has Brachydactyly type E (https://rarediseases.info.nih.gov/diseases/987/brachydactyly-type-e)')
